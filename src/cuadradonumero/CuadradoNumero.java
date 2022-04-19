3a9937be0d9d5dd6aea8ccc0d5cc8fbb668a0422
andrescala13 <andres2214119@correo.uis.edu.co>
2022-04-19 16:25:14 -0500

    Desarrollo: Programa completo en java.andrescala13

---
+++ src/cuadradonumero/CuadradoNumero.java
@@ -0,0 +1,24 @@
+/*hallar el cuadrado de un numero usando la siguiente relacion N2= 1+3+5+...+(2N-1)
+ejemplo = N=3 n2= 9
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuadradonumero;
+
+/**
+ *
+ * @author andre
+ */
+public class CuadradoNumero 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
